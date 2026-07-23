e1f6fc683a5b2dec9ec462e4006acbc03e990b36
Samruddhi Bhabad <104028204+Sam-1806@users.noreply.github.com>
2026-07-23 15:44:06 -0400

    Time: 561 ms | Memory: 0B - LeetPush

---
+++ 1757-recyclable-and-low-fat-products/solution.sql
@@ -0,0 +1,9 @@
+-- Recyclable and Low Fat Products
+-- Difficulty: Easy
+-- Runtime: 561 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+# Write your MySQL query statement below
+select product_id from Products where low_fats='Y' and 
+recyclable='Y'
